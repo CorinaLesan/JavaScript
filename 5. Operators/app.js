75f2376bb051e525a462b230b614b044d4f10b11
Corina Lesan <corinalesan06gmail.com>
2025-11-23 14:54:44 +0200

    5. Operators

---
+++ 5. Operators/app.js
@@ -0,0 +1,17 @@
+const a = 2;
+const b = 3.14;
+const c = 'hello';
+const d = '5';
+const e = 9;
+
+console.log('a + b =', a + b);
+console.log('b - a =', b - a);
+console.log('a * b =', a * b);
+console.log('b / a =', b / a);
+console.log('b % a =', b % a);
+console.log('e % a =', e % a);
+
+console.log('a + c', a + c);
+console.log('c - a =', c - a);
+console.log('a + d =', a + d);
+console.log('d - a =', d - a);
