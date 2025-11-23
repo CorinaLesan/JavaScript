35d5e2241da2fe9038dcda7f385dee72d727e9fc
Corina Lesan <corinalesan06gmail.com>
2025-11-23 14:54:14 +0200

    2. Variables and constants

---
+++ 2. Variables and constants/app.js
@@ -0,0 +1,17 @@
+var a = 4;
+let b = 5;
+
+console.log('a=', a);
+console.log('b=', b);
+
+a = 10;
+b = 15;
+
+console.log('a=', a);
+console.log('b=', b);
+
+const c = 20;
+console.log('c=', c);
+
+c = 25; // Это приведет к ошибке. Поскольку константы не меняются
+console.log('c=', c);
